a996e8bc06eef4229f010f3e7a4a5be41e5b7188
JoelFaldin <joelfaldin@gmail.com>
2025-09-30 20:29:25 -0300

    refactor: moving maplibre logic into service

--- src/app/home/map/map.ts
+++ src/app/home/map/map.ts
@@ -1,6 +1,8 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import maplibregl from 'maplibre-gl';
+
+import { MapService } from '../../map-service';
 
 @Component({
   selector: 'app-map',
@@ -10,14 +12,11 @@ import maplibregl from 'maplibre-gl';
 })
 export class Map {
   @ViewChild('mapContainer', { static: false }) mapContainer!: ElementRef<HTMLDivElement>;
+  mapService: MapService = inject(MapService);
 
   ngAfterViewInit() {
-    const map = new maplibregl.Map({
-      container: this.mapContainer.nativeElement,
-      style: 'http://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/openStreetMap.json',
-      center: [-70.6483, -33.4569],
-      zoom: 20,
-    });
+    // Map centered on Santiago, Chile:
+    const map = this.mapService.startMap(this.mapContainer, [-70.6483, -33.4569], 6);
 
     map.addControl(new maplibregl.NavigationControl(), 'top-right');
   }
---
+++ src/app/map-service.ts
@@ -0,0 +1,29 @@
+import { ElementRef, Injectable } from '@angular/core';
+import maplibregl from 'maplibre-gl';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapService {
+  private map!: maplibregl.Map;
+  private styleUrl: string = 'http://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/openStreetMap.json';
+
+  startMap(mapContainer: ElementRef<HTMLDivElement>, center: [number, number] = [0, 0], zoom: number = 2): maplibregl.Map {
+    this.map = new maplibregl.Map({
+      container: mapContainer.nativeElement,
+      style: this.styleUrl,
+      center: center,
+      zoom: zoom,
+    });
+
+    return this.map;
+  }
+
+  setCenter(center: [number, number]) {
+    this.map.setCenter(center);
+  }
+
+  setZoom(zoom: number) {
+    this.map.setZoom(zoom);
+  }
+}
