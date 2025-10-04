9f513d75116c8f0c6eba5b26f0dd43a8535e92eb
JoelFaldin <joelfaldin@gmail.com>
2025-10-04 00:22:26 -0300

    feat: improving looks of geojson upload input and processing its contents

--- src/app/modal/modal.ts
+++ src/app/modal/modal.ts
@@ -8,7 +8,22 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class Modal {
   @Output() close = new EventEmitter<void>();
 
-  onFileSelected(event: any) {
-    console.log('test')
+  onFileSelected(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const file = target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const target = e.target?.result as string;
+        const geoJson = JSON.parse(target).features;
+        console.log(geoJson);
+      };
+
+      reader.readAsText(file);
+
+      this.close.emit();
+    }
   }
 }
